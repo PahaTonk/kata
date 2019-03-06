8523060c5b6e29cac7c7c7332a887155fc8d9889
Pavel <gavrikboss@gmail.com>
2019-03-06 15:00:34 +0300

    Пофиксил баг с поиском расширения

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-let solve = files => { 
-    let set = new Set(); 
-    let map = new Map(); 
+var solve = files => { 
+    files = files.map(item => item.match(/\.\w+$/)[0]);
+    let arr1 = [];
+    let arr2 = []; 
     while (files.length > 0) {
         let i = files[0]; 
         let count = 0; 
-        while (files.indexOf(i) !== -1) { 
-            set.add(i);
+        while (files.indexOf(i) !== -1) {
             files.splice(files.indexOf(i), 1);
             ++count;
         }
-        map.set(i, count);
+        arr1.push(i);
+        arr2.push(count);
     }
-    console.log(set);
-    console.log(map);
+    let arr =[];
+    let val = Math.max.apply(null, arr2);
+    for (let j = arr1.length - 1; j > -1; j--) {
+        if (val == arr2[j]) {
+            arr.push(arr1[j])
+        }
+    }
+    return arr;
 } 
 
-solve(['index.html','style.css','main.js','style.css','main.js','style.css']);
+solve(['index.html','style.css','main.js','style.css','main.js','main.js','style.css']);
